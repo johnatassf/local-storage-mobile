5cb93e603e34dbd974cd7f89b87e681b810b555e
Alan Monteiro <alan.hiwatari@gmail.com>
2020-05-30 02:24:31 -0300

    Componentes Visuais

--- src/app/cadastro-contato/cadastro-contato.page.ts
+++ src/app/cadastro-contato/cadastro-contato.page.ts
@@ -13,51 +13,37 @@ import { EnumAction } from '../home/enum-action';
   styleUrls: ['./cadastro-contato.page.scss'],
 })
 export class CadastroContatoPage implements OnInit {
-
-
   private form: FormGroup;
   public contatos: Contato[] = [];
   id: number;
-  title: string
+  title: string;
   constructor(private formBuilder: FormBuilder, public navCtrl: NavController, private route: ActivatedRoute) {
-
     this.contatos = JSON.parse(localStorage.getItem('contatos'));
   }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      'nome': ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(15),
-        
-      ])],
-      'telefone': ['', Validators.compose([
-        Validators.required,
-        Validators.pattern(/^[1-9]{2}[0-9]{4,5}[0-9]{4}$/)
-      ])],
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(15)])],
+      telefone: ['', Validators.compose([Validators.required, Validators.pattern(/^[1-9]{2}[0-9]{4,5}[0-9]{4}$/)])],
     });
 
     if (this.route.snapshot.data['action'] === EnumAction.Update) {
-      this.route.params.subscribe(params => {
+      this.route.params.subscribe((params) => {
         this.id = +params['id'];
         this.loadContato(this.id);
       });
 
       this.title = 'Atualizar Contato';
-    }else{
+    } else {
       this.title = 'Cadastrar Contato';
     }
-    
-
   }
 
   salvarValores() {
-
     const id = this.contatos.length == null ? 1 : this.contatos.length + 1;
-    
+
     if (this.route.snapshot.data['action'] === EnumAction.Update) {
-      this.contatos = this.contatos.filter(c =>  c.id !== this.id);
+      this.contatos = this.contatos.filter((c) => c.id !== this.id);
 
       this.contatos.push({ id: this.id, nome: this.form.value.nome, telefone: this.form.value.telefone });
 
@@ -65,20 +51,19 @@ export class CadastroContatoPage implements OnInit {
     } else {
       this.contatos.push({ id, nome: this.form.value.nome, telefone: this.form.value.telefone });
     }
-    
+
     localStorage.setItem('contatos', JSON.stringify(this.contatos));
     this.goToHome();
   }
 
-    loadContato(id: number) {
-      var contato = this.contatos.find(c => c.id === id);
+  loadContato(id: number) {
+    var contato = this.contatos.find((c) => c.id === id);
 
-      this.form.controls.nome.patchValue(contato.nome);
-      this.form.controls.telefone.patchValue(contato.telefone);
-    }
-
-    goToHome(){
-      this.navCtrl.navigateRoot('home');
-    }
-
+    this.form.controls.nome.patchValue(contato.nome);
+    this.form.controls.telefone.patchValue(contato.telefone);
   }
+
+  goToHome() {
+    this.navCtrl.navigateRoot('home');
+  }
+}
